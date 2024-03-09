5eff31289cdef31064b4747be7932fb4079bb093
epic-person-on <123142178+epic-person-on@users.noreply.github.com>
2024-03-08 20:31:23 -0800

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,70 @@
-    // Function to handle search button click
-    function redirectToSearch() {
-      var searchTerm = document.getElementById('search').value;
-      // Redirect to search page with query parameter
-      window.location.href = '#gsc.tab=0&gsc.q=' + encodeURIComponent(searchTerm);
-    }
+var config = {
+  gcseId: 'f4b47a42c2ba04b72',
+  resultsUrl: 'https://hoopsquad.sytes.net',
+  searchWrapperClass: 'gcse-search-wrapper',
+  resultsWrapperClass: 'gcse-results-wrapper'
+};
 
-    // Add event listener to search button
-    document.getElementById('search').addEventListener('keydown', function(event) {
-      if (event.key === "Enter") {
-        redirectToSearch();
+var renderSearchForms = function () {
+  if (document.readyState == 'complete') {
+    queryAndRender();
+  } else {
+    google.setOnLoadCallback(function () {
+      queryAndRender();
+    }, true);
+  }
+};
+
+var queryAndRender = function() {
+  var gsceSearchForms = document.querySelectorAll('.' + config.searchWrapperClass);
+  var gsceResults = document.querySelectorAll('.' + config.resultsWrapperClass);
+
+  if (gsceSearchForms) {
+    renderSearch(gsceSearchForms[0]);
+  }
+  if (gsceResults) {
+    renderResults(gsceResults[0]);
+  }
+};
+
+var renderSearch = function (div) {
+    google.search.cse.element.render(
+      {
+        div: div.id,
+        tag: 'searchbox-only',
+        attributes: {
+          resultsUrl: config.resultsUrl
+        }
       }
+    );
+    if (div.dataset &&
+        div.dataset.stylingFunction &&
+        window[div.dataset.stylingFunction] &&
+        typeof window[div.dataset.stylingFunction] === 'function') {
+      window[div.dataset.stylingFunction](form);
+    }
+};
+
+var renderResults = function(div) {
+  google.search.cse.element.render(
+    {
+      div: div.id,
+      tag: 'searchresults-only'
     });
+};
+
+window.__gcse = {
+  parsetags: 'explicit',
+  callback: renderSearchForms
+};
+
+(function () {
+  var cx = config.gcseId;
+  var gcse = document.createElement('script');
+  gcse.type = 'text/javascript';
+  gcse.async = true;
+  gcse.src = (document.location.protocol == 'https:' ? 'https:' : 'http:') +
+    '//cse.google.com/cse.js?cx=' + cx;
+  var s = document.getElementsByTagName('script')[0];
+  s.parentNode.insertBefore(gcse, s);
+})();
